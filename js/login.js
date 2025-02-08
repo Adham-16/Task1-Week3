c3bb2ee797bdf5afda22fd8681c4cdada0ec263f
Adham Mohamed <85314873+Adham-16@users.noreply.github.com>
2025-02-08 20:40:32 +0200

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,9 +1,13 @@
-if (localStorage.getItem("loggedIn") === "true" && window.location.pathname.includes("login.html")) {
+
+
+// ***********************************************************************
+if (localStorage.getItem("loggedIn") === "true") {
   window.location.href = "index.html";
 }
 
 document.getElementById("loginForm").addEventListener("submit", function (event) {
   event.preventDefault();
+  
   const email = document.getElementById("email").value.trim();
   const password = document.getElementById("password").value.trim();
   const storedUser = JSON.parse(localStorage.getItem("user"));
